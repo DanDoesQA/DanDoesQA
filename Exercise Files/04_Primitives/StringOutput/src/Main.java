483cbf768f8eeeeb9d8a34f5bd0c393d57b97b7b
Dan Snellenbarger <dsnellenbarger@linkedin.com>
2015-10-06 13:16:56 -0700

    New  Hello World via LDC

---
+++ Exercise Files/04_Primitives/StringOutput/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char c = 'z';
+		boolean bool = true;
+		byte b = 127;
+		short s = 32000;
+		int i = 2000000;
+		long l = 10000000L;
+		float f = 1234245.435234f;
+		double d = 112312312331.34;
+	}
+
+}
---
+++ Exercise Files/Solutions/10_Inheritance/AbstractClasses/src/com/lynda/olivepress/olives/Kalamata.java
@@ -0,0 +1,25 @@
+package com.lynda.olivepress.olives;
+
+public class Kalamata extends Olive {
+
+	public Kalamata() {
+		super(2);
+		this.name = "Kalamata";
+		this.flavor = "Grassy";
+		this.color = Olive.BLACK;
+	}
+	
+	@Override
+	public int crush() {
+		System.out.println("crush from subclass");
+		return super.crush();
+	}
+
+	@Override
+	public String getOrigin() {
+		return "Greece";
+	}
+
+	
+	
+}
